54a8347d06aeac4a5a97844cd8827899959f2559
jaeverba-javana <javier.e.ver.ba.code@gmail.com>
2024-07-03 22:03:13 -0500

    :art: feat(server): reaady to develop
    He quitado cosas por defecto, para asi comenzar a programar

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import type {RouteRecordRaw} from "vue-router";
+
+export const routes:RouteRecordRaw[] = [
+    // {
+    //     path: '/',
+    //     name: 'HomeLayout',
+    //     component: () => import('@/layouts/Main.layout.vue'),
+    //     meta: {
+    //         title: "Home",
+    //         meta: [
+    //             {
+    //                 name: "description",
+    //                 content: "esta es mi breve description"
+    //             }
+    //         ]
+    //     }
+    // }
+]
--- utils/constants.js
+++ utils/constants.js
@@ -4,3 +4,29 @@ export const IS_PRODUCTION = process.env.NODE_ENV === 'production'
 export const BASE = process.env.BASE || '/'
 // eslint-disable-next-line no-undef
 export const PORT = process.env.PORT || IS_PRODUCTION ? 80 : 5173
+
+export const CONSOLE_COLORS = {
+    reset: "\x1b[0m",
+    bright: "\x1b[1m",
+    dim: "\x1b[2m",
+    underscore: "\x1b[4m",
+    blink: "\x1b[5m",
+    reverse: "\x1b[7m",
+    hidden: "\x1b[8m",
+    fg_black: "\x1b[30m",
+    fg_red: "\x1b[31m",
+    fg_green: "\x1b[32m",
+    fg_yellow: "\x1b[33m",
+    fg_blue: "\x1b[34m",
+    fg_magenta: "\x1b[35m",
+    fg_cyan: "\x1b[36m",
+    fg_white: "\x1b[37m",
+    bg_black: "\x1b[40m",
+    bg_red: "\x1b[41m",
+    bg_green: "\x1b[42m",
+    bg_yellow: "\x1b[43m",
+    bg_blue: "\x1b[44m",
+    bg_magenta: "\x1b[45m",
+    bg_cyan: "\x1b[46m",
+    bg_white: "\x1b[47m"
+}
