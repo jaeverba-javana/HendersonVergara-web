7d01631d571a23ac6b3fc3ed3d28fcd5bf2f1a05
jaeverba-javana <javier.e.ver.ba.code@gmail.com>
2024-12-31 17:51:57 -0500

    :mag: other(SEO): canonical page added

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,6 +15,12 @@ export const routes:RouteRecordRaw[] = [
                     name: "description",
                     content: "Henderson Vergara, fotógrafo profesional en Santa Marta, Colombia, especializado en bodas en la playa. Capturamos momentos memorables con servicios de fotografía y video de alta calidad. Documentamos cada detalle de tus eventos especiales, creando recuerdos eternos. Contacta a Henderson Vergara Producciones para un servicio personalizado y profesional que superará tus expectativas."
                 }
+            ],
+            link: [
+                {
+                    rel: "canonical",
+                    href: "https://hendersonvergara.com/"
+                }
             ]
         }
     }
